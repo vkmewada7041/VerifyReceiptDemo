5d78523acfeaa7da3946f4ce9bc8a752cdcb880f
vkmewada <vkmewada7041@gmail.com>
2020-04-26 10:13:15 +0530

    Initial Commit

---
+++ VerifyReceiptDemo/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  VerifyReceiptDemo
+//
+//  Created by vasishth suthar on 26/04/20.
+//  Copyright © 2020 technicalinitial. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
